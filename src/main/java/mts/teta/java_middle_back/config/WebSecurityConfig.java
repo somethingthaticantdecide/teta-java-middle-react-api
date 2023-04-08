ea2f49c3b5d4d23da57eb85254c9c27c8a10e4ba
somethingthaticantdecide <somethingthaticantdecide@gmail.com>
2023-04-09 01:37:30 +0300

    init

--- src/main/java/mts/teta/java_middle_back/config/WebSecurityConfig.java
+++ src/main/java/mts/teta/java_middle_back/config/WebSecurityConfig.java
@@ -4,53 +4,47 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home", "/api/auth").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .loginPage("/api/auth")
-                        .permitAll()
-                );
-
-        return http.build();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user1 = User
-                .withUsername("user1")
-                .password(passwordEncoder().encode("user1Pass"))
-                .roles("USER")
-                .build();
-        UserDetails user2 = User
-                .withUsername("user2")
-                .password(passwordEncoder().encode("user2Pass"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User
-                .withUsername("admin")
-                .password(passwordEncoder().encode("adminPass"))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user1, user2, admin);
-    }
-
-    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public StrictHttpFirewall httpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowedHeaderValues((header) -> true);
+        return firewall;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/signUp", "/api/auth").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .rememberMe()
+                    .tokenValiditySeconds(3600)
+                .and()
+                    .formLogin()
+                    .loginPage("/api/auth")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .deleteCookies("JSESSIONID")
+                    .deleteCookies("remember-me")
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .logoutSuccessUrl("/");
+        return http.build();
     }
 }
--- src/main/java/mts/teta/java_middle_back/controller/AuthController.java
+++ src/main/java/mts/teta/java_middle_back/controller/AuthController.java
@@ -1,13 +1,39 @@
 package mts.teta.java_middle_back.controller;
 
+import org.json.JSONObject;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@CrossOrigin
 @RestController
 public class AuthController {
-//    @GetMapping("/api/auth")
-//    public String auth() {
-//
-//        return "";
-//    }
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/api/auth")
+    public String doGet(HttpServletRequest request) {
+        JSONObject token = new JSONObject();
+        token.put("token", "token");
+        JSONObject data = new JSONObject();
+        data.put("data", token);
+        return data.toString();
+    }
+
+    @PostMapping
+    public String doPost(HttpServletRequest request, Model model) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        model.addAttribute("invalidPass", true);
+        return "signIn";
+    }
 }
