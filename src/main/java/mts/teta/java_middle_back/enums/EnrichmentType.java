5a0a36a065ad9aacc323002a7b21246a8a28721d
aasaparin <aasaparin@sberbank.ru>
2023-05-19 18:58:48 +0300

    +

---
+++ src/main/java/mts/teta/java_middle_back/enums/EnrichmentType.java
@@ -0,0 +1,5 @@
+package mts.teta.java_middle_back.enums;
+
+public enum EnrichmentType {
+    MSISDN;
+}
--- src/main/java/mts/teta/java_middle_back/model/Message.java
+++ src/main/java/mts/teta/java_middle_back/model/Message.java
@@ -1,11 +1,9 @@
 package mts.teta.java_middle_back.model;
 
+import mts.teta.java_middle_back.enums.EnrichmentType;
+
 public class Message {
     private String content;
     private String msisdn;
     private EnrichmentType enrichmentType;
-
-    public enum EnrichmentType {
-        MSISDN;
-    }
 }
