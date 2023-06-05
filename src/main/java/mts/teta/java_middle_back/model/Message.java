a32a01f5a1d3f984f08db0ba249dec148c8f36f7
somethingthaticantdecide <somethingthaticantdecide@gmail.com>
2023-06-05 11:25:25 +0300

    next one

--- src/main/java/mts/teta/java_middle_back/model/Message.java
+++ src/main/java/mts/teta/java_middle_back/model/Message.java
@@ -1,9 +1,13 @@
 package mts.teta.java_middle_back.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import mts.teta.java_middle_back.enums.EnrichmentType;
 
+@JsonIgnoreProperties
 public class Message {
     private String content;
+    private String action;
+    private String page;
     private String msisdn;
     private EnrichmentType enrichmentType;
 }
--- src/main/java/mts/teta/java_middle_back/services/EnrichmentService.java
+++ src/main/java/mts/teta/java_middle_back/services/EnrichmentService.java
@@ -2,11 +2,12 @@ package mts.teta.java_middle_back.services;
 
 import mts.teta.java_middle_back.model.Message;
 import mts.teta.java_middle_back.validators.MessageValidator;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.stereotype.Service;
+import sun.jvm.hotspot.utilities.Assert;
 
 @Service
 public class EnrichmentService {
-    // принцип DI НЕ нарушен
     private final MessageValidator validator;
 
     public EnrichmentService(MessageValidator validator) {
@@ -14,6 +15,12 @@ public class EnrichmentService {
     }
 
     public String enrich(Message message) {
+//        Assert.that(message, );
+        return "";
+    }
+
+    public String enrich(String message) {
+//        JSONAssert.assertNotEquals();
         return "";
     }
 }
